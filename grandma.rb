4550fb44e0b21944ade36e4c4af1a55a4e3eb737
Ryan McCahon <mccahonr@yahoo.com>
2019-08-13 20:47:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
 
-  if phrase == "I LOVE YOU GRANDMA!"
+  if  phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
     response =  "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    response  == "NO, NOT SINCE 1938"  
   else
     response = "HUH?! SPEAK UP, SONNY!"
   end
